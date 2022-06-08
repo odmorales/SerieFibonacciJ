20643b8236e961f85b3e6dc4ae87e6e107a52160
OdMorales <OdMorales@DESKTOP-MH402MR>
2022-06-07 20:34:26 -0500

    creacion proyecto

---
+++ src/Logica/SerieF.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logica;
+
+/**
+ *
+ * @author OdMorales
+ */
+public class SerieF {
+    
+    public int[] getSerie(){
+        
+        return new int[0];
+    }
+    
+}
